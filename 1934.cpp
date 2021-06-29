a1c4ebd9e71b5fff0c51b507b52356cac4a89c6f
kimhyeonwokk <66130064+kimhyeonwokk@users.noreply.github.com>
2021-06-29 21:22:51 +0900

    Create 1934.cpp

---
+++ 1934.cpp
@@ -0,0 +1,21 @@
+// 1934
+#include <iostream>
+using namespace std;
+int gcd(int a, int b) {
+	int r;
+	while (b != 0) {
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+int main() {
+	int n;
+	int a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		cout << a * b / gcd(a, b) << endl;
+	}
+}
