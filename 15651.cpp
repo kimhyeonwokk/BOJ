479dcc324f60844af033f766918c80f744fc6877
kimhyeonwokk <66130064+kimhyeonwokk@users.noreply.github.com>
2021-07-02 19:20:33 +0900

    Create 15651.cpp

---
+++ 15651.cpp
@@ -0,0 +1,27 @@
+// 15651번  N과 M (3)
+#include <iostream>
+#define max 8
+using namespace std;
+int N, M;
+int v1[max] = { 0 };
+void find(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << v1[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		v1[cnt] = i;
+		find(cnt + 1);
+	}
+
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> M;
+	find(0);
+}
