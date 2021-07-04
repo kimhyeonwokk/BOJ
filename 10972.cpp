0b30c0aa025c78ec15821eab88e0727b39553888
kimhyeonwokk <66130064+kimhyeonwokk@users.noreply.github.com>
2021-07-04 18:13:17 +0900

    Create 10972.cpp

---
+++ 10972.cpp
@@ -0,0 +1,26 @@
+// 10972 다음 순열
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+	int N;
+	cin >> N;
+	vector<int> v1;
+	for (int i = 1; i <= N; i++) {
+		int temp;
+		cin >> temp;
+		v1.push_back(temp);
+	}
+	if (next_permutation(v1.begin(), v1.end()) == 0) {
+		cout << -1 << endl;
+		return 0;
+	}
+	for (auto i : v1) {
+		cout << i << " ";
+	}
+	return 0;
+}
